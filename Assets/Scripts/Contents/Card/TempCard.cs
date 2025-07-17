01e733e07eca7971081112fd5a1ee9a2af78536e
leejk206 <ljk9121@naver.com>
2025-07-17 16:48:07 +0900

    merge
    merge

---
+++ Assets/Scripts/Contents/Card/TempCard.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TempCard : CardBase
+{
+
+
+    public override void OnCardDestroyEffect()
+    {
+
+    }
+
+    public override void OnCardDrawEffect()
+    {
+
+    }
+
+    public override void OnNoteTriggerEffect()
+    {
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
