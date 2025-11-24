84756e29fc2ac23135b6e7339bfbd10593521eae
leejk206 <ljk9121@naver.com>
2025-11-24 19:46:26 +0900

    장미 전쟁 카드 프리팹 적용
    장미 전쟁 카드 프리팹 적용

--- Assets/Scripts/Contents/Card/War Of The Roses/Red/RedRoseJokerDiamond.cs
+++ Assets/Scripts/Contents/Card/War Of The Roses/Red/RedRoseJokerDiamond.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class RedRoseJokerSpade : WaroftheRosesBase
+public class RedRoseJokerDiamond : WaroftheRosesBase
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
