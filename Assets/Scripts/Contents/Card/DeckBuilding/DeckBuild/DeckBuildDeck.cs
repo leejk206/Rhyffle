0835d5dc9c05dcdba74692ba8227484d1ed995e2
cueqlp <euncue0905@gmail.com>
2026-01-09 01:48:07 +0900

    임시저장

--- Assets/Scripts/Contents/Card/DeckBuilding/DeckBuild/DeckBuildDeck.cs
+++ Assets/Scripts/Contents/Card/DeckBuilding/DeckBuild/DeckBuildDeck.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DeckBuildDeck : MonoBehaviour
@@ -6,6 +8,7 @@ public class DeckBuildDeck : MonoBehaviour
     public RectTransform rect;
     public RectTransform canvas;
     public GameObject deckBuildSlot;
+    public List<GameObject> cardInDeck;
 
     #region sizeInfo
     float scrWid;
@@ -13,6 +16,7 @@ public class DeckBuildDeck : MonoBehaviour
     float boardHei;
     float boardWid;
     float boardY;
+    float boardX;
     float cardHei;
     float cardWid;
     float cardY;
@@ -30,7 +34,7 @@ public class DeckBuildDeck : MonoBehaviour
 
     public void NewDeck()
     {
-
+        SetBoard();
     }
 
     private void Start()
@@ -44,19 +48,32 @@ public class DeckBuildDeck : MonoBehaviour
         scrWid = canvas.GetComponent<RectTransform>().rect.width;
         scrHei = canvas.GetComponent<RectTransform>().rect.height;
         boardHei = scrHei / 3;
-        boardWid = scrWid;
-        boardY = (scrHei / 16 * 3);
-        rect.sizeDelta = new Vector2(boardWid, boardHei);
-        rect.localPosition = new Vector3(0, boardY, 0);
-
         cardHei = boardHei / 5 * 4;
         cardWid = (cardHei * 5) / 8;
 
+        boardWid = cardWid * 56 + cardWid * 5.7f;
+        
+        boardY = (scrHei / 16 * 3);
+        boardX = boardWid / 2 - scrWid/2;
+
+        leftLimit = boardWid/2 - scrWid/2;
+        rightLimit = - boardWid/2 + scrWid/2;
+
+        rect.sizeDelta = new Vector2(boardWid, boardHei);
+        rect.localPosition = new Vector3(boardX, boardY, 0);
+
+
         cardY = boardY;
 
-        for(int i = 0; i < 52; i++)
+        for(int i = 0; i < 56; i++)
         {
-            cardX = (boardHei / 20) + cardWid / 2;
+            cardX = -cardWid * (i - 27.5f) * 1.1f;
+            GameObject temp = Instantiate(deckBuildSlot);
+            temp.transform.SetParent(transform);
+            temp.GetComponent<RectTransform>().localPosition = new Vector2(cardX, 0);
+            temp.GetComponent<RectTransform>().sizeDelta = new Vector2(cardWid, cardHei);
+            temp.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
+            cardInDeck.Add(temp);
         }
     }
 }
