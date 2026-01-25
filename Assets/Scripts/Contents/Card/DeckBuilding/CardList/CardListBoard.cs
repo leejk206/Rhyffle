e6f90c7e38195c37fde780c369f201958ba486df
cueqlp <euncue0905@gmail.com>
2026-01-26 03:02:04 +0900

    feat/Deckbuilding 스크립트

---
+++ Assets/Scripts/Contents/Card/DeckBuilding/CardList/CardListBoard.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System;
+
+public class CardListBoard : MonoBehaviour
+{
+    //하나의 CardBoard에 들어갈 카드 열 수
+    // BoardPos shows this board is before or current or next board showing up
+    public int boardPos = 0;
+    public int cardsPerLine;
+    public List<GameObject> cardOnBoard;
+    public GameObject cardListCard;
+    RectTransform rect;
+
+    #region sizeInfo
+    float scrWid;
+    float scrHei;
+    float boardHei;
+    float boardWid;
+    float boardY;
+    float cardHei;
+    float cardWid;
+    float cardY;
+    float leftMostCardPos;
+    float cardX;
+    #endregion
+
+    public void SetBoard()
+    {
+        rect = gameObject.GetComponent<RectTransform>();
+        scrWid = gameObject.transform.parent.parent.gameObject.GetComponent<RectTransform>().rect.width;
+        scrHei = gameObject.transform.parent.parent.gameObject.GetComponent<RectTransform>().rect.height;
+        boardHei = scrHei / 6 * 5;
+        boardWid = scrWid / 10 * 9;
+        boardY = - (scrHei /24 );
+        rect.sizeDelta = new Vector2(boardWid, boardHei);
+        rect.localPosition = new Vector3(0, boardY, 0);
+
+        cardHei = boardHei / 40 * 17;
+        cardWid = (cardHei * 5) / 8;
+        cardsPerLine = Mathf.FloorToInt((boardWid - (boardHei / 20))/(cardWid + (boardHei/20)));
+
+
+        cardY = boardHei / 40 + cardHei / 2; ;
+
+        leftMostCardPos = boardWid/2 - boardHei/10 - cardWid / 2;
+
+        for (int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < cardsPerLine; j++)
+            {
+                cardX = -leftMostCardPos + (j * (leftMostCardPos * 2) / (cardsPerLine - 1));
+                GameObject temp = Instantiate(cardListCard);
+                temp.transform.SetParent(gameObject.transform);
+                temp.GetComponent<RectTransform>().localPosition = new Vector2(cardX, cardY * (- 2* i + 1));
+                temp.GetComponent<RectTransform>().sizeDelta = new Vector2(cardWid, cardHei);
+                temp.GetComponent<RectTransform>().localScale = new Vector2(1,1);
+                cardOnBoard.Add(temp);
+            }
+        }
+        Move(boardPos);
+    }
+    public void Move(int pos)
+    {
+        rect.localPosition = new Vector3(pos * transform.parent.gameObject.GetComponent<RectTransform>().rect.width, boardY,0);
+    }
+
+    public void ApplyCard(List<DeckCardInfo> cards)
+    {
+
+        for(int i = 0; i < cards.Count; i++)
+        {
+            cardOnBoard[i].SetActive(true);
+            cardOnBoard[i].GetComponent<CardListCard>().SetCard(cards[i]);
+        }
+        for(int i = cards.Count; i <cardsPerLine * 2; i++)
+        {
+            cardOnBoard[i].SetActive(false);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Utils/Card.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class CardPage
+{
+    public int card_page;
+    public DeckCardInfo[] card_index;
+}
+
+[Serializable]
+public class DeckCardInfo
+{
+    public int card_baseid;
+    public int pack_id;
+    public int card_particle_id;
+    public int ability_id1;
+    public int ability_id2;
+    public int ability_id3;
+    public int ability_level1;
+    public int ability_level2;
+    public int ability_level3;
+    public int unique_ability_level;
+    public bool bookmarked;
+}
+[Serializable]
+public class CardBasePage
+{
+    public int card_base_page;
+    public CardBaseInfo[] card_index;
+}
+
+[Serializable]
+public class CardBaseInfo {
+    public int card_baseid;
+    public string card_name;
+    public Define.CardRank card_rank;
+    public Define.CardRarity card_rarity;
+    public Define.CardSuit card_suit;
+    public string collections;
+    public int unique_ability_id;
+}
+
